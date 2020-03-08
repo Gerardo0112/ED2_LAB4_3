e1d8aa4b42d5e6aedff7b7ec37b78a1b5c6256c2
unknown <gerar0112@gmail.com>
2020-03-07 23:48:25 -0600

    Definicion de diccionarios.

--- ED2_LAB4_3/Models/Huffman_A.cs
+++ ED2_LAB4_3/Models/Huffman_A.cs
@@ -7,5 +7,13 @@ namespace ED2_LAB4_3.Models
 {
     public class Huffman_A
     {
+        private Node root;
+        private Dictionary<byte, string> prefixes = new Dictionary<byte, string>();
+        public Dictionary<string, byte> descompression_prefixes = new Dictionary<string, byte>();
+
+        public Dictionary<byte, string> dictionary()
+        {
+            return prefixes;
+        }
     }
 }
