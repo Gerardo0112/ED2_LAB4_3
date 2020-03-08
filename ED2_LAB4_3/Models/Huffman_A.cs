34f67366ae4e236a7c10f6ce425a802a4886edaf
unknown <gerar0112@gmail.com>
2020-03-08 00:35:37 -0600

    Añadir prefijos.

--- ED2_LAB4_3/Models/Huffman_A.cs
+++ ED2_LAB4_3/Models/Huffman_A.cs
@@ -10,12 +10,10 @@ namespace ED2_LAB4_3.Models
         private Node root;
         private Dictionary<byte, string> prefixes = new Dictionary<byte, string>();
         public Dictionary<string, byte> descompression_prefixes = new Dictionary<string, byte>();
-
         public Dictionary<byte, string> dictionary()
         {
             return prefixes;
         }
-
         Node node_(Node less, Node higher)
         {
             var father = new Node();
@@ -26,7 +24,6 @@ namespace ED2_LAB4_3.Models
             father.data.probability = (less.data.probability + higher.data.probability);
             return father;
         }
-
         public void Huffman(List<Node> characters)
         {
             while (characters.Count > 1)
@@ -49,6 +46,27 @@ namespace ED2_LAB4_3.Models
             root = characters[0];
             characters.Clear();
             characters = null;
+        }
+        public void asign_prefix(Node actual, string prefix)
+        {
+            if(actual.right != null)
+            {
+                if(actual.left != null)
+                {
+                    asign_prefix(actual.right, prefix + "0");
+                    asign_prefix(actual.left, prefix + "1");
+                }
+            }
+            else
+            {
+                actual.data.prefix_c = prefix;
+                prefixes.Add(actual.data.c, actual.data.prefix_c);
+            }
+        }
+        public Dictionary<byte, string> fill()
+        {
+            asign_prefix(root, "");
+            return prefixes;
         }
     }
 }
