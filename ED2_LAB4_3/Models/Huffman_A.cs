302327a5799869955223c1cef1b50252b4b52dca
unknown <gerar0112@gmail.com>
2020-03-08 00:24:05 -0600

    Armar el nodo y ajustar huffman.

--- ED2_LAB4_3/Models/Huffman_A.cs
+++ ED2_LAB4_3/Models/Huffman_A.cs
@@ -15,5 +15,40 @@ namespace ED2_LAB4_3.Models
         {
             return prefixes;
         }
+
+        Node node_(Node less, Node higher)
+        {
+            var father = new Node();
+            var info_father = new Reading();
+            father.data = info_father;
+            father.left = less;
+            father.right = higher;
+            father.data.probability = (less.data.probability + higher.data.probability);
+            return father;
+        }
+
+        public void Huffman(List<Node> characters)
+        {
+            while (characters.Count > 1)
+            {
+                var node = new Node();
+                node = node_(characters[0], characters[1]);
+                characters.RemoveAt(0);
+                characters.RemoveAt(0);
+
+                var new_n = characters.FindIndex(x => x.data.probability >= node.data.probability);
+                if (new_n != -1)
+                {
+                    characters.Insert(new_n, node);
+                }
+                else
+                {
+                    characters.Add(node);
+                }
+            }
+            root = characters[0];
+            characters.Clear();
+            characters = null;
+        }
     }
 }
