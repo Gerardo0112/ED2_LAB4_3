ad59fb3ca053562f14299c47b5cd620a51d4db48
unknown <gerar0112@gmail.com>
2020-03-07 20:16:54 -0600

    Identificación clase Compression.

--- ED2_LAB4_3/Models/Compression.cs
+++ ED2_LAB4_3/Models/Compression.cs
@@ -7,5 +7,10 @@ namespace ED2_LAB4_3.Models
 {
     public class Compression
     {
+        public string file { get; set; }
+        public string route { get; set; }
+        public double reason_c { get; set; }
+        public double factor_c { get; set; }
+        public double reduction_percentage { get; set; }
     }
 }
