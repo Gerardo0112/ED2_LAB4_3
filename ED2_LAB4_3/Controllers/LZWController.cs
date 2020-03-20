20463ff10477e412e2bbef476c4e658ef40925e6
unknown <gerar0112@gmail.com>
2020-03-20 00:04:14 -0600

    Controlador y clase de LZW.

--- ED2_LAB4_3/Controllers/LZWController.cs
+++ ED2_LAB4_3/Controllers/LZWController.cs
@@ -2,41 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ED2_LAB4_3.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class LZWController : ControllerBase
     {
-        // GET api/values
+        // GET: api/LZW
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        // GET: api/LZW/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST: api/LZW
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT: api/LZW/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
---
+++ ED2_LAB4_3_PARTE2/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ED2_LAB4_3_PARTE2
+{
+    public class Class1
+    {
+    }
+}
